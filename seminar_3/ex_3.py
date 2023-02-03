43e42e2df35c626a5a4d42aea903b3ae43e003f2
Yuri E <196240@gmail.com>
2023-02-03 14:12:25 +0100

    ex3 added

---
+++ seminar_3/ex_3.py
@@ -0,0 +1,15 @@
+#Задайте список из вещественных чисел. Напишите программу, 
+#которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+import random
+xlist = []
+for i in range(random.randint(1,10)):
+    n = round(random.uniform(0, 10), 2)
+    xlist.append(n)
+print(xlist)
+ylist = []
+for i in range(len(xlist)):
+    ylist.append(round(xlist[i]*100%100))
+print('Max: ', max(ylist))
+print('Min: ', min(ylist))
+print('Max - Min: ', max(ylist) - min(ylist))
